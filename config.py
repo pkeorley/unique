1da916ba22cfcf053331a53e9331c3ee97364dad
pkeorley <59422798+pkeorley@users.noreply.github.com>
2022-04-13 14:11:24 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+data = {
+    "secret_key": "putingeyandpidorasebaniy",
+    "mongo_client": "mongodb+srv://peaky:228YM228@Cluster0.k4fzh.mongodb.net/peakydb?retryWrites=true&w=majority",
+    "run": {
+        "debug": True,
+        "threaded": True,
+        "port": 5000
+    }
+}
