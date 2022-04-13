05bc6944f455da8ed07071c7f3364a8ed7156e0e
pkeorley <59422798+pkeorley@users.noreply.github.com>
2022-04-13 14:09:57 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import asyncio
+
+from quart import Quart
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+
+from config import data
+
+
+app = Quart(__name__)
+app.config["SECRET_KEY"] = data["secret_key"]
+app = MongoClient(data["mongo_client"])
+mongo.get_io_loop = asyncio.get_running_loop
+
+
+@app.route("/", methods=["GET"])
+async def index():
+    return "<h1>Hello, world!</h1>"
+
+
+@app.route("/config")
+async def secretkey():
+    return data
+
+
+if __name__ == "__main__":
+    run = data["run"]
+    app.run(
+        debug=run["debug"],
+        threaded=run["threaded"],
+        port=run["port"]
+    )
