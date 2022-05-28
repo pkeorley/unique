9390ffabcfa471b5b8488cb7fccd59793609aa4a
pkeorley <59422798+pkeorley@users.noreply.github.com>
2022-05-29 00:51:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,9 @@ users = client.website.users
 
 @app.route("/")
 def index():
-	message = users.find_one({})
+	message = users.find_one({})["messages"]
 	return render_template("index.html",
-		last_message_content=message["last_message_content"].replace("&", "").replace("<", "").replace(">", ""),
-		last_message_id=message["last_message_id"],
-		last_message_created_at=message["last_message_created_at"]
+		messages=messages[::-1]
 		)
 
 if __name__ == "__main__":
