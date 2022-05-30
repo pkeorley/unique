ec6cc192c932485221bc1267ab998d4101c2a2f5
pkeorley <59422798+pkeorley@users.noreply.github.com>
2022-05-31 00:43:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -246,7 +246,7 @@ def api_docs():
     <hr>
         <span><h3>POST <div style="color: #00ff00;">/shortlink/create</h3></div></span>
         <p>In order to use the link building, you need to enter the key, the data to which you will be redirected, and the api key</p>
-        <span><div style="color: #ff0000;">Example:</div> <b><code>requests.post('http://www.pkeorley.ml/api/v1/shortlink/create?', json={'key': 'google', 'url': 'https://google.com/', 'api_key': '{api_key}'})</code></b></span>
+        <span><div style="color: #ff0000;">Example:</div> <b><code>requests.post('http://www.pkeorley.ml/api/v1/shortlink/create?', json=\{'key': 'google', 'url': 'https://google.com/', 'api_key': '{api_key}'\})</code></b></span>
     """
 if __name__ == "__main__":
     app.run(**data["run"])
