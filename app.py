31633e9ddfb10cdbfcb22be51ecfdd58608269f5
pkeorley <59422798+pkeorley@users.noreply.github.com>
2022-05-30 18:46:30 +0300

    update app.py

--- app.py
+++ app.py
@@ -62,25 +62,48 @@ def api_create_invite():
                         "key": args[0],
                         "url": args[1],
                         "api_key": args[1]
-                    }
+                    },
+                    "solution": "Введите все 3 аргумента"
                 },
                 "example": "http://www.pkeorley.ml/api/invite/create/?key=google&url=https://google.com/&api_key=pLQNGMyCclqOOEUD"
             })
         elif all(args) is True:
-            if not invites.count_documents({
-            
+            if invites.count_documents({
+                "type": "api_key",
+                "key": request.args["api_key"]
             }) == 0:
                 return jsonify({
                     "error": {
-                        "text": "Данный ключ уже есть в базе данных"
+                        "text": "Не верный api_key",
+                        "solution": "Попросите разработчика новый api_key"
                     }
                 })
-            else:
-                invites.update_one(invite, {"$inc": {
-                    "used": 1,
-                    "uses": -1
-                }})
-                
+            elif invites.find_one({
+                "type": "api_key",
+                "key": request.args["api_key"]
+            })["uses"] <= 0:
+                return jsonify({
+                    "error": {
+                        "text": "Недостаточно использований апи-ключа (0)",
+                        "solution": "Попросите разработчика выдать вам использования"
+                    }
+                })
+   
+            invites.update_one({
+                "type": "api_key",
+                "key": request.args["api_key"]
+            }, {"$inc": {
+                "used": 1,
+                "uses": -1
+            }})
+            invites.insert_one({
+                "type": "invite",
+                "key": request.args["key"],
+                "url": request.args["url"]
+            })
+            return jsonify({
+                "result": f"http://www.pkeorley.ml/invite/{request.args['key']}"
+            })
 
 
 if __name__ == "__main__":
